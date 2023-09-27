162d8bcddc96084d4931640185b0d91ec04a76b4
andesson reis <andessonreys@gmail.com>
2023-09-27 14:03:16 -0300

    Initial commit

---
+++ anilibrary/src/main/java/com/anilibrary/anilibrary/AnilibraryApplication.java
@@ -0,0 +1,13 @@
+package com.anilibrary.anilibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnilibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnilibraryApplication.class, args);
+	}
+
+}
---
+++ anilibrary/src/test/java/com/anilibrary/anilibrary/AnilibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anilibrary.anilibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnilibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
