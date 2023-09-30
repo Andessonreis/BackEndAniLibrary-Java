e8df9c32171f9c3c6d79f8eadb8b029dff688231
andesson reis <andessonreys@gmail.com>
2023-09-30 19:53:56 -0300

    feat: Criado controller com DTO para transporte

---
+++ anilibrary/src/main/java/com/anilibrary/anilibrary/controllers/AnimeDto.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.anilibrary.anilibrary.controllers;
+
+import lombok.Data;
+
+/**
+ *
+ * @author andesson
+ */
+@Data
+public class AnimeDto {
+
+    private Long id;
+
+    private String title;
+
+    private String imageUrl;
+
+    private String synopsis;
+}
